ce05f963a2d40dc0eb9167c985449ea7c73fd227
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-11 21:39:50 -0300

    Adicionar arquivos de projeto.

---
+++ PiagetMvc/Data/PiagetMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PiagetMvc.Models;
+
+namespace PiagetMvc.Data
+{
+    public class PiagetMvcContext : DbContext
+    {
+        public PiagetMvcContext (DbContextOptions<PiagetMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PiagetMvc.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
---
+++ PiagetMvc/Models/Base.cs
@@ -0,0 +1,7 @@
+﻿namespace PiagetMvc.Models
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+    }
+}
